5587b6d4f5cc23c8008ecc132b3c4fe739e89d95
12Rolli <106396250+12Rolli@users.noreply.github.com>
2022-08-31 10:26:45 +0200

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -7,50 +7,56 @@ const cssnano = require('cssnano');
 const babel = require('gulp-babel');
 const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
+//const imagemin = require('gulp-imagemin');
 
 // Sass Task
 function scssTask() {
-	return src('app/scss/style.scss', { sourcemaps: true })
-		.pipe(sass())
-		.pipe(postcss([autoprefixer(), cssnano()]))
-		.pipe(dest('dist', { sourcemaps: '.' }));
+    return src('app/scss/style.scss', { sourcemaps: true })
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(dest('dist', { sourcemaps: '.' }));
 }
 
 // JavaScript Task
 function jsTask() {
-	return src('app/js/script.js', { sourcemaps: true })
-		.pipe(babel({ presets: ['@babel/preset-env'] }))
-		.pipe(terser())
-		.pipe(dest('dist', { sourcemaps: '.' }));
+    return src('app/js/script.js', { sourcemaps: true })
+        .pipe(babel({ presets: ['@babel/preset-env'] }))
+        .pipe(terser())
+        .pipe(dest('dist', { sourcemaps: '.' }));
 }
+
+//Image Task
+//function imgTask() {
+//    return src('images/*').pipe(imagemin()).pipe(gulp.dest('dist/images'));
+//}
 
 // Browsersync
 function browserSyncServe(cb) {
-	browsersync.init({
-		server: {
-			baseDir: '.',
-		},
-		notify: {
-			styles: {
-				top: 'auto',
-				bottom: '0',
-			},
-		},
-	});
-	cb();
+    browsersync.init({
+        server: {
+            baseDir: '.',
+        },
+        notify: {
+            styles: {
+                top: 'auto',
+                bottom: '0',
+            },
+        },
+    });
+    cb();
 }
 function browserSyncReload(cb) {
-	browsersync.reload();
-	cb();
+    browsersync.reload();
+    cb();
 }
 
 // Watch Task
 function watchTask() {
-	watch('*.html', browserSyncReload);
-	watch(
-		['app/scss/**/*.scss', 'app/**/*.js'],
-		series(scssTask, jsTask, browserSyncReload)
-	);
+    watch('*.html', browserSyncReload);
+    watch(
+        ['app/scss/**/*.scss', 'app/**/*.js'],
+        series(scssTask, jsTask, browserSyncReload)
+    );
 }
 
 // Default Gulp Task
