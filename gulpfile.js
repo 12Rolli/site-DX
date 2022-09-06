88afe170552836de25bc3014941a653c2d277294
12Rolli <106396250+12Rolli@users.noreply.github.com>
2022-09-06 18:13:36 +0200

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 // Initialize modules
-const { src, dest, watch, series } = require('gulp');
+const { src, dest, watch, series, task } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
@@ -7,7 +7,8 @@ const cssnano = require('cssnano');
 const babel = require('gulp-babel');
 const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
-//const imagemin = require('gulp-imagemin');
+const imagemin = require('gulp-imagemin');
+const gulp = require('gulp')
 
 // Sass Task
 function scssTask() {
@@ -25,10 +26,10 @@ function jsTask() {
         .pipe(dest('dist', { sourcemaps: '.' }));
 }
 
-//Image Task
-// function imgTask() {
-//     return src('images/*').pipe(imagemin()).pipe(gulp.dest('dist/images'));
-// }
+// Image Task
+function imgTask() {
+    return src('images/**/*').pipe(imagemin()).pipe(dest('dist/images'));
+}
 
 // Browsersync
 function browserSyncServe(cb) {
@@ -50,14 +51,42 @@ function browserSyncReload(cb) {
     cb();
 }
 
+task('copy-assets', series(
+    function() {
+        return src(['index.html']).pipe(dest('dist'));
+    },
+    function() {
+        return src(['app/pages/**/*']).pipe(dest('dist'));
+    },
+    scssTask,
+    jsTask,
+    imgTask
+));
+
+task('dry', series(
+    'copy-assets',
+    function() {
+        return src(['dist/**/*']).pipe(dest('/Users/ronald/project/digitalx/site/tmp'));
+    }
+));
+
+task('deploy', series(
+    'copy-assets',
+    function() {
+        return src(['dist/**/*']).pipe(dest('/var/www/digital-x-sarl.com/html'));
+    }
+));
+
+
+
 // Watch Task
 function watchTask() {
     watch('*.html', browserSyncReload);
     watch(
         ['app/scss/**/*.scss', 'app/**/*.js', 'images/*'],
-        series(scssTask, jsTask, browserSyncReload)
+        series(scssTask, jsTask, imgTask, browserSyncReload)
     );
 }
 
 // Default Gulp Task
-exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+exports.default = series(scssTask, jsTask, imgTask, browserSyncServe, watchTask);
